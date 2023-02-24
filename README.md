4604a602d11d613885f8bb1b969152bce06dddd5
mrflick72 <valerio.vaudi@gmail.com>
2023-02-24 21:00:15 +0100

    setup script gitignore

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 # local-environment
 
-The project is composed by a docker-compose yml designed to provide you all the basic infrastructure to start vauthenticator app in local.
+The project is composed by a docker-compose yml designed to provide you all the basic infrastructure to start
+vauthenticator app in local.
 
 # local host config
 
-add on your local hosts file the following configurations
+- add on your local hosts file the following configurations
 
-127.0.0.1   local.assets.vauthenticator.com
-127.0.0.1   local.api.vauthenticator.com
-127.0.0.1   local.management.vauthenticator.com
+    ```
+    127.0.0.1   local.assets.vauthenticator.com
+    127.0.0.1   local.api.vauthenticator.com
+    127.0.0.1   local.management.vauthenticator.com
+    ```
+- make sure that you have a clean installation 
+  - docker-compose down  
+  - docker-compose rm  
+  - clean if already there the docker folder on the project root. In this folder will be placed the dynamodb storage 
 
+- run the setup.sh
+  ```
+  After that the setup.sh is executed in the project root you should have a file called `kms.logs`. 
+  The content of this file is the master key to insert in the configuration
+  file [vauthenticator.yml](configuration%2Fvauthenticator.yml).
+  Property name is: `key.master-key: ${A_MASTE R_KEY}`
+  ```
--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 VAUTHENTICATOR_BUCKET="vauthenticator"
 TEMPLATES=("welcome.html" "mail-verify-challenge.html" "reset-password.html" "successful-mail-verify.html" "mfa-challenge.html")
 
+export AWS_ACCESS_KEY_ID=xxx
+export AWS_SECRET_ACCESS_KEY=xxx
+export AWS_DEFAULT_REGION=us-east-1
+
+export DYNAMO_DB_ENDPOINT=http://localhost:4566
+export KMS_ENDPOINT=http://localhost:8000
+
 rm -rf tenant-installer
 
 sudo chown -R  $USER *
@@ -11,19 +18,22 @@ cd tenant-installer/terraform/resources
 
 cat ../../../terraform-ovverrides/terraform.tf > terraform.tf
 cat ../../../terraform-ovverrides/s3.tf > s3.tf
-aws s3api create-bucket --bucket $VAUTHENTICATOR_BUCKET --endpoint http://localhost:4566 --region us-east-1  2>&1 > ../../../s3.logs
+aws s3api create-bucket --bucket $VAUTHENTICATOR_BUCKET --endpoint http://localhost:4566 2>&1 > ../../../s3.logs
 
 terraform init
 terraform plan -var-file=../../../terraform-ovverrides/variables.tfvars
 terraform apply -var-file=../../../terraform-ovverrides/variables.tfvars -auto-approve
 
-terraform state show  'module.kms.aws_kms_key.this[0]' | grep key_id | awk  '{print $3}' > ../../../kms.logs
+terraform state show  'module.kms.aws_kms_key.this[0]' | grep key_id | awk  '{print $3}' | sed  s/\"//g  > ../../../kms.logs
+MASTER_KEY=$(cat ../../../kms.logs)
 
 cd ../../document/template/mail
 for TEMPLATE in ${TEMPLATES[@]}
 do
-  aws s3 cp $TEMPLATE s3://$VAUTHENTICATOR_BUCKET/mail/templates/$TEMPLATE --endpoint http://localhost:4566 --region us-east-1
+  aws s3 cp $TEMPLATE s3://$VAUTHENTICATOR_BUCKET/mail/templates/$TEMPLATE --endpoint http://localhost:4566
 done
 
+
 cd ../../../scripts
-python setup.py admin@email.com
+pip3 install -r requirements.txt
+python3 setup.py admin@email.com $MASTER_KEY  VAuthenticator_Signature_Keys_Local_Staging  VAuthenticator_Role_Local_Staging VAuthenticator_Account_Local_Staging  VAuthenticator_Account_Role_Local_Staging VAuthenticator_ClientApplication_Local_Staging
