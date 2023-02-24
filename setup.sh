51d31f882761245896d8176c26225f439bdf4fe8
mrflick72 <valerio.vaudi@gmail.com>
2023-02-24 01:44:45 +0100

    configurations

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+VAUTHENTICATOR_BUCKET="vauthenticator"
+TEMPLATES=("welcome.html" "mail-verify-challenge.html" "reset-password.html" "successful-mail-verify.html" "mfa-challenge.html")
+
 rm -rf tenant-installer
 
 sudo chown -R  $USER *
@@ -8,8 +11,19 @@ cd tenant-installer/terraform/resources
 
 cat ../../../terraform-ovverrides/terraform.tf > terraform.tf
 cat ../../../terraform-ovverrides/s3.tf > s3.tf
-aws s3api create-bucket --bucket vauthenticator --endpoint http://localhost:4566 --region us-east-1  2>&1 > s3.logs
+aws s3api create-bucket --bucket $VAUTHENTICATOR_BUCKET --endpoint http://localhost:4566 --region us-east-1  2>&1 > ../../../s3.logs
 
 terraform init
 terraform plan -var-file=../../../terraform-ovverrides/variables.tfvars
 terraform apply -var-file=../../../terraform-ovverrides/variables.tfvars -auto-approve
+
+terraform state show  'module.kms.aws_kms_key.this[0]' | grep key_id | awk  '{print $3}' > ../../../kms.logs
+
+cd ../../document/template/mail
+for TEMPLATE in ${TEMPLATES[@]}
+do
+  aws s3 cp $TEMPLATE s3://$VAUTHENTICATOR_BUCKET/mail/templates/$TEMPLATE --endpoint http://localhost:4566 --region us-east-1
+done
+
+cd ../../../scripts
+python setup.py admin@email.com
